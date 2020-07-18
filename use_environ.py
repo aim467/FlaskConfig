0a46c762e030520613ac674ec4470fca9c929a4b
aim467 <709544711@qq.com>
2020-07-18 15:34:41 +0800

    add function in use_environ.py

--- use_environ.py
+++ use_environ.py
@@ -6,11 +6,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def is_set():
+    if not os.environ.get("FLASK_CONFIG"):
+        return True
+    elif not os.path.exists("settings.py"):
+        return True
+    else:
+        return False
+
 # 设置环境变量
 os.environ.setdefault("Flask_Config", "settings.py")
 
 # 从环境变量中获取配置文件名
-app.config.from_envvar("Flask_Config")
+app.config.from_envvar("Flask_Config", silent=is_set())
 
 
 @app.route("/")
